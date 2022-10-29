6e5add927c061ab58438cede7a2d2ba1d6a1ad7f
zend <luizz.ggr@gmail.com>
2022-10-29 17:26:33 -0300

    Platform Repo

---
+++ PlataformServiceAPI/Repositories/PlatformRepo.cs
@@ -0,0 +1,36 @@
+﻿using PlataformServiceAPI.Data;
+using PlataformServiceAPI.Models;
+
+namespace PlataformServiceAPI.Repositories;
+
+public class PlatformRepo : IPlatformRepo
+{
+    private readonly AppDbContext ctx;
+
+    public PlatformRepo(AppDbContext ctx)
+    {
+        this.ctx = ctx;
+    }
+
+    public void CreatePlatform(Platform plat)
+    {
+        if (plat is null) throw new ArgumentNullException(nameof(plat));
+
+        ctx.Platforms.Add(plat);
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        return ctx.Platforms.ToList();
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+        return ctx.Platforms.FirstOrDefault(p => p.Id == id);
+    }
+
+    public bool SaveChanges()
+    {
+        return ctx.SaveChanges() >= 0;
+    }
+}

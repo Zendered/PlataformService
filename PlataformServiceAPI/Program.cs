abd08f59ba4a854282f52cd8836c3afd8eefc954
zend <luizz.ggr@gmail.com>
2022-10-29 17:26:04 -0300

    config

--- PlataformServiceAPI/Program.cs
+++ PlataformServiceAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PlataformServiceAPI.Data;
+using PlataformServiceAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Scope
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+#endregion
+
+# region ImMemoryDb
+//var connection = builder.Configuration.GetConnectionString();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("ImMem"));
+#endregion
 
 var app = builder.Build();
 

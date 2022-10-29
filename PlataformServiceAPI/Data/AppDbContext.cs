fca4ad1700b80ade67f2fbdf63c3ac96d444f740
zend <luizz.ggr@gmail.com>
2022-10-29 16:45:08 -0300

    Add project files.

---
+++ PlataformServiceAPI/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlataformServiceAPI.Models;
+
+namespace PlataformServiceAPI.Data;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<Plataform> Plataforms { get; set; }
+
+	public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+	{
+
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Plataform>()
+			.Property(c => c.Name)
+			.HasMaxLength(100)
+			.IsRequired();
+
+		modelBuilder.Entity<Plataform>()
+			.Property(c => c.Publisher)
+			.HasMaxLength(100)
+			.IsRequired();
+
+		modelBuilder.Entity<Plataform>()
+			.Property(c => c.Cost)
+			.IsRequired();
+	}
+}
---
+++ PlataformServiceAPI/Models/Plataform.cs
@@ -0,0 +1,9 @@
+﻿namespace PlataformServiceAPI.Models;
+
+public class Plataform
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
+    public string Cost { get; set; } = string.Empty;
+}

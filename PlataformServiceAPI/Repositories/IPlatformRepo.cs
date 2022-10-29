ac34024136ea226624a86f319582f7a539d71be7
zend <luizz.ggr@gmail.com>
2022-10-29 17:26:25 -0300

    Platform Repo Interface

---
+++ PlataformServiceAPI/Repositories/IPlatformRepo.cs
@@ -0,0 +1,11 @@
+﻿using PlataformServiceAPI.Models;
+
+namespace PlataformServiceAPI.Repositories;
+
+public interface IPlatformRepo
+{
+    void CreatePlatform(Platform plat);
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformById(int id);
+    bool SaveChanges();
+}

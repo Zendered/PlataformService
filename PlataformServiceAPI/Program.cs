589235bcae90455ea127b098cec4dd7a239d4a33
zend <luizz.ggr@gmail.com>
2022-11-02 18:00:32 -0300

    adding automapper config

--- PlataformServiceAPI/Program.cs
+++ PlataformServiceAPI/Program.cs
@@ -5,13 +5,14 @@ using PlataformServiceAPI.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 #region Scope
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 #endregion
 

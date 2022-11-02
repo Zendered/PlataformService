d0faaa72d60065922ff70c59778c88e3ac354bce
zend <luizz.ggr@gmail.com>
2022-11-02 18:09:05 -0300

    platforms profile

---
+++ PlataformServiceAPI/Profiles/PlatformProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PlataformServiceAPI.Dtos;
+using PlataformServiceAPI.Models;
+
+namespace PlataformServiceAPI.Profiles;
+
+public class PlatformProfile : Profile
+{
+	public PlatformProfile()
+	{
+		CreateMap<Platform, PlatformCreateDto>().ReverseMap();
+		CreateMap<Platform, PlatformReadDto>().ReverseMap();
+	}
+}

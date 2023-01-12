1ea848a041c331a5edac3a60475abf9b3f8d7d59
zend <luizz.ggr@gmail.com>
2023-01-12 14:22:29 -0300

    Command Read Dto

---
+++ CommandsServiceAPI/Dtos/CommandReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CommandsServiceAPI.Dtos;
+
+public class CommandReadDto
+{
+    public int Id { get; set; }
+    public int PlatformId { get; set; }
+    public string HowTo { get; set; } = string.Empty;
+    public string CommandLine { get; set; } = string.Empty;
+}

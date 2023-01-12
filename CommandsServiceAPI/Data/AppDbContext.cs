2fb0d1873ab1ee60c71631686b707597d2e4545a
zend <luizz.ggr@gmail.com>
2023-01-12 14:11:53 -0300

    Db context for command service

---
+++ CommandsServiceAPI/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CommandsServiceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsServiceAPI.Data;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<Platform> Platforms { get; set; }
+	public DbSet<Command> Commands { get; set; }
+
+	public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+	{
+
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder
+			.Entity<Platform>()
+			.HasMany(p => p.Commands)
+			.WithOne(p => p.Platform)
+			.HasForeignKey(p => p.PlatformId);
+
+		modelBuilder
+			.Entity<Command>()
+			.HasOne(p => p.Platform)
+			.WithMany(p => p.Commands)
+			.HasForeignKey(p => p.PlatformId);
+	}
+}

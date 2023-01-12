0853baa1144a197e4b32dc4e9d96843bf2d41174
zend <luizz.ggr@gmail.com>
2023-01-12 14:12:32 -0300

    command/platform model for commands service

---
+++ CommandsServiceAPI/Models/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsServiceAPI.Models;
+
+public class Platform
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public int ExternalId { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Command> Commands { get; set; } = new List<Command>();
+}

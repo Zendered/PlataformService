e7833f6ca630ac3286763f297f51108b1ceb2f9b
zend <luizz.ggr@gmail.com>
2023-01-12 14:15:33 -0300

    Command Repo interface

---
+++ CommandsServiceAPI/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+﻿using CommandsServiceAPI.Models;
+
+namespace CommandsServiceAPI.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChanges();
+
+    // platform
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform plat);
+    bool PlatformExists(int platformId);
+
+    // command
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+}

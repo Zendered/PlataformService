32ae76b6223557bc41d821031ee9f1068d1c2bc4
zend <luizz.ggr@gmail.com>
2023-01-12 14:36:12 -0300

    adding automapper

---
+++ CommandsServiceAPI/Profiles/CommandsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CommandsServiceAPI.Dtos;
+using CommandsServiceAPI.Models;
+
+namespace CommandsServiceAPI.Profiles;
+
+public class CommandsProfile : Profile
+{
+	public CommandsProfile()
+	{
+		//platforms
+		CreateMap<Platform, PlatformReadDto>();
+
+		//commands
+		CreateMap<CommandCreateDto, Command>();
+		CreateMap<Command, CommandReadDto>();
+	}
+}
